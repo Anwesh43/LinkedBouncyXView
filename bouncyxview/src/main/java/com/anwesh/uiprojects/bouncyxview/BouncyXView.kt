4866ccdd854a2f9261aee292d4e9ef38e58e2fe3
Anwesh43 <anweshthecool0@gmail.com>
2020-03-22 16:56:15 +0530

    adding constants and libraries

--- bouncyxview/src/main/java/com/anwesh/uiprojects/bouncyxview/BouncyXView.kt
+++ bouncyxview/src/main/java/com/anwesh/uiprojects/bouncyxview/BouncyXView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.bouncyxview
 /**
  * Created by anweshmishra on 22/03/20.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
